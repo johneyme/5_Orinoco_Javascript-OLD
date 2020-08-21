b4fb4124af0950b98299ef3f2e4e75938d76f96b
John <john.eyme@gmail.com>
2020-08-21 14:46:10 +0200

    insertion card

--- javascript/index.js
+++ javascript/index.js
@@ -11,12 +11,32 @@ const getProduct = async function () {
       
       function templateItems(data) {
           const cardItems = document.getElementById('items')
-          const divRow = document.createElement("row")
-          const divCol = document.createElement("row")
-          const divRow = document.createElement("row")
+          
+          const divRow = document.createElement('div');
+          divRow.setAttribute('class', 'row')
+          cardItems.appendChild(divRow)
+
+          const divCol = document.createElement('div');
+          divCol.setAttribute('class', 'col-12 col-lg-4')
+          divRow.appendChild(divCol)
+
+          const card = document.createElement('div');
+          card.setAttribute('class', 'card')
+          divCol.appendChild(card)
+
+          const cardBody = document.createElement('div');
+          cardBody.setAttribute('class', 'card-body')
+          card.appendChild(cardBody)
+
+          const cardTitle = document.createElement('div');
+          cardTitle.setAttribute('class', 'card-title')
+          cardBody.appendChild(cardTitle)
+          const titleProd = document.createElement('h5')
+          titleProd.textContent = data.name
+          cardTitle.appendChild(titleProd)
 
          
-      }
+        }
       
       for (let i = 0; i < data.length; i++) {
         templateItems(data[i]);
