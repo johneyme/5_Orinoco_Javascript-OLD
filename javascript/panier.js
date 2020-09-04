5d74e012441d037c7075050d3d6dfeb06546c23a
Eyme <john.eyme@gmail.com>
2020-09-04 16:44:59 +0200

    modif apres mentorat 04/09/20
    modification ajout panier JS produit + rectification recap panier + mothod POST fonctionnel

--- javascript/panier.js
+++ javascript/panier.js
@@ -1,207 +1,209 @@
-    function articlesPanier (){
-        
-        
-
-        
-        if(localStorage.length === 0){
-          location.replace('http://127.0.0.1:5500/html/paniervide.html')
-        }
+function articlesPanier() {
 
 
-        
-        const rowId = document.getElementById('contenupanier')
-        const divCol = document.createElement('div')
-        divCol.setAttribute('class', 'col-md-8 order-md-2 mb-4')
-        rowId.appendChild(divCol)
+    let total = 0
 
-
-        const panierTitle = document.createElement('h4')
-        panierTitle.setAttribute('class', 'd-flex justify-content-between align-items-center mb-3')
-        panierTitle.innerHTML = '<span class="text-muted">Panier</span>'
-        divCol.appendChild(panierTitle)
-
-        const ulList = document.createElement('ul')
-        ulList.setAttribute('class', "list-group mb-3")
-        divCol.appendChild(ulList)
-
-      
-    
-
-        let product = ""
-
-        const cartInformation = {
-            contact: {},
-            products: []
+    if (localStorage.length === 0) {
+        location.replace('http://127.0.0.1:5500/html/paniervide.html')
     }
-        for (var i = 0; i < localStorage.length; i++) {
+
+
+
+    const rowId = document.getElementById('contenupanier')
+    const divCol = document.createElement('div')
+    divCol.setAttribute('class', 'col-md-8 order-md-2 mb-4')
+    rowId.appendChild(divCol)
+
+
+    const panierTitle = document.createElement('h4')
+    panierTitle.setAttribute('class', 'd-flex justify-content-between align-items-center mb-3')
+    panierTitle.innerHTML = '<span class="text-muted">Panier</span>'
+    divCol.appendChild(panierTitle)
+
+    const ulList = document.createElement('ul')
+    ulList.setAttribute('class', "list-group mb-3")
+    divCol.appendChild(ulList)
+
+
+
+
+    let product = ""
+
+    const cartInformation = {
+        contact: {},
+        products: []
+    }
+    for (let i = 0; i < localStorage.length; i++) {
+
+        products = JSON.parse(localStorage.getItem(('cartProducts')))
+        console.log(product)
+        products.forEach((product, index) => {
             
-            product = JSON.parse(localStorage.getItem(localStorage.key(i))) 
-            const nomProd = product[1]
-            const priceProd = product[2] 
-            const idProd = product[0]   
-        
-        const liList = document.createElement('li')
-        liList.setAttribute('class', 'list-group-item d-flex justify-content-between lh-condensed')
-        ulList.appendChild(liList)
+            total = total + (product.price * product.quantity)
 
-        const div = document.createElement('div')
-        liList.appendChild(div)
+            const nomProd = product.name
+            const priceProd = product.price
+            const quantProd = product.quantity
+            const idProd = product._id
+            sousTotal = (quantProd * priceProd)
+
+            const liList = document.createElement('li')
+            liList.setAttribute('class', 'list-group-item d-flex justify-content-between lh-condensed')
+            ulList.appendChild(liList)
+
+            const div = document.createElement('div')
+            liList.appendChild(div)
 
 
-        const itemTitle = document.createElement('h6')
-        itemTitle.setAttribute("class", 'my-0')
-        itemTitle.textContent = nomProd
-        div.appendChild(itemTitle)
+            const itemTitle = document.createElement('h6')
+            itemTitle.setAttribute("class", 'my-0')
+            itemTitle.textContent = `${nomProd} | Quantité: ${quantProd}`
+            div.appendChild(itemTitle)
 
-        const price = document.createElement('span')
-        price.setAttribute('class', 'text-muted')
-        price.textContent = `${Math.round(priceProd/6.5597)} €` 
-        liList.appendChild(price)
-        
-        
-        cartInformation.products.push(idProd)
-        
-            };
-        
+            const price = document.createElement('span')
+            price.setAttribute('class', 'text-muted')
+            price.textContent = `${Math.round(priceProd / 6.5597)} €/pce = ${Math.round(sousTotal / 6.5597)} €`
+            liList.appendChild(price)
 
-       
-        
-        console.log(localStorage)
-                
-       
-        const liPrice = document.createElement('li')
-        liPrice.setAttribute('class', 'list-group-item d-flex justify-content-between')
-        liPrice.innerHTML = `<span>Total (EUR)</span><strong>0 €</strong>`
-        ulList.appendChild(liPrice)
+            cartInformation.products.push(idProd)
 
-        const butVider = document.getElementById('viderpanier')
-        butVider.addEventListener('click', function() {
-        localStorage.clear()
-        butVider.textContent = "Veuillez patienter ..."
-        window.setTimeout(function(){location.replace("paniervide.html")}, 2000)
         })
         
-    
-        const prenomId = document.getElementById('prenom')
-        const nomId = document.getElementById('nom')
-        const adresseId = document.getElementById('adresse')
-        const villeId = document.getElementById('ville')
-        const emailId = document.getElementById('email')
-        const btnCommande = document.getElementById('confirmercommande')
-        
 
-        const validationForm = () => {
+    };
 
-            const isNotEmpty = value => value !== ""  ? true:false;
-            const isLongEnough = value => value.length >= 2 ? true:false;
-            const containNumber = /[0-9]/;
-            const doNotContainNumber = value => !value.match(containNumber) ? true : false;
-            const specialCharacter = /[$£°&+,:;=?@#|'<>.^*()!"{}_]/
-            const doNotContainSpecialCharacter = value => !value.match(specialCharacter) ? true: false;
-            const regexEmail = /.+@.+\..+/
-            const isValidEmail = (value) => value.match(regexEmail)? true : false;
-            const isValidInput = (value) => isNotEmpty(value) && isLongEnough(value) && doNotContainNumber(value) && doNotContainSpecialCharacter(value);
+console.log(cartInformation)
+
+
+    console.log(localStorage)
+
+
+    const liPrice = document.createElement('li')
+    liPrice.setAttribute('class', 'list-group-item d-flex justify-content-between')
+    liPrice.innerHTML = `<span>Total (EUR)</span><strong>${Math.round(total / 6.5597)} €</strong>`
+    ulList.appendChild(liPrice)
+
+    const butVider = document.getElementById('viderpanier')
+    butVider.addEventListener('click', function () {
+        localStorage.clear()
+        butVider.textContent = "Veuillez patienter ..."
+        window.setTimeout(function () { location.replace("paniervide.html") }, 2000)
+    })
+
+
+    const prenomId = document.getElementById('prenom')
+    const nomId = document.getElementById('nom')
+    const adresseId = document.getElementById('adresse')
+    const villeId = document.getElementById('ville')
+    const emailId = document.getElementById('email')
+    const btnCommande = document.getElementById('confirmercommande')
+
+
+    const validationForm = () => {
+
+        const isNotEmpty = value => value !== "" ? true : false;
+        const isLongEnough = value => value.length >= 2 ? true : false;
+        const containNumber = /[0-9]/;
+        const doNotContainNumber = value => !value.match(containNumber) ? true : false;
+        const specialCharacter = /[$£°&+,:;=?@#|'<>.^*()!"{}_]/
+        const doNotContainSpecialCharacter = value => !value.match(specialCharacter) ? true : false;
+        const regexEmail = /.+@.+\..+/
+        const isValidEmail = (value) => value.match(regexEmail) ? true : false;
+        const isValidInput = (value) => isNotEmpty(value) && isLongEnough(value) && doNotContainNumber(value) && doNotContainSpecialCharacter(value);
 
         /*const prenomErrorMessage = document.getElementById('prenominvalide')
         const nomErrorMessage = document.getElementById('prenominvalide')
         const adresseMessage = document.getElementById('prenominvalide')
         const villeErrorMessage = document.getElementById('prenominvalide')
         const emailErrorMessage = document.getElementById('prenominvalide')*/
-            const prenom = prenomId.value
-            const nom = nomId.value
-            const adresse = adresseId.value
-            const ville = villeId.value
-            const email = emailId.value
+        const prenom = prenomId.value
+        const nom = nomId.value
+        const adresse = adresseId.value
+        const ville = villeId.value
+        const email = emailId.value
 
-          if(isValidInput(prenom)) {
-              true
-          } else {
-              prenomId.setAttribute('placeholder', 'Prenom requis !')
-              return false
-          }
+        if (isValidInput(prenom)) {
+            true
+        } else {
+            prenomId.setAttribute('placeholder', 'Prenom requis !')
+            return false
+        }
 
-          if(isValidInput(nom)) {
-              true
-          } else {
-              nomId.setAttribute('placeholder', 'Nom requis !')
-              return false
-          }
+        if (isValidInput(nom)) {
+            true
+        } else {
+            nomId.setAttribute('placeholder', 'Nom requis !')
+            return false
+        }
 
-          if(isNotEmpty(adresse) && isLongEnough(adresse)) {
-              true
-          } else {
-              adresseId.setAttribute('placeholder', 'Adresse requise !')
-              return false
-          }
-          if(isValidInput(ville)) {
-              true
-          } else {
+        if (isNotEmpty(adresse) && isLongEnough(adresse)) {
+            true
+        } else {
+            adresseId.setAttribute('placeholder', 'Adresse requise !')
+            return false
+        }
+        if (isValidInput(ville)) {
+            true
+        } else {
             villeId.setAttribute('placeholder', 'Ville requise !')
             return false
-          }
+        }
 
-          if(isValidEmail(email)) {
-              true
-          } else {
+        if (isValidEmail(email)) {
+            true
+        } else {
             emailId.setAttribute('placeholder', 'Email requis !')
             return false
-          }
-
-          return cartInformation.contact = {
-                "prenom": prenom,
-                "nom": nom,
-                "adresse": adresse,
-                "ville": ville,
-                "email": email
-                }
         }
 
-        const postData = async (method, url, dataElt) =>{
-            const response = await fetch (url, {
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                method,
-                body: JSON.stringify(dataElt)
-            })
-            return await response.json();
+        return cartInformation.contact = {
+            "firstName": prenom,
+            "lastName": nom,
+            "address": adresse,
+            "city": ville,
+            "email": email
         }
-       
-
-        btnCommande.addEventListener('click', async function(e){
-            e.preventDefault()
-            if(validationForm()){
-            
-        btnCommande.textContent = "Veuillez patienter ..."
-        const formValid = validationForm()
-        if (formValid !== false){
-            const response = await postData('POST', 'http://localhost:3000/api/cameras/order', cartInformation)
-            //window.setTimeout(function(){location.replace("confirmation.html")}, 2000)
-        }
-    } else {
-        validationForm()
-        btnCommande.textContent = "Veuillez remplir correctement les champs ..."
     }
-        //localStorage.clear()
-        
-        console.log(cartInformation)
-            })
 
+    console.log(cartInformation)
+    console.log(JSON.stringify(cartInformation))
+
+
+
+    const postData = async (method, url, dataElt) => {
+        const response = await fetch(url, {
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            method,
+            body: JSON.stringify(dataElt)
+        })
+        return await response.json();
+    }
+
+
+    btnCommande.addEventListener('click', async function (e) {
+        e.preventDefault()
+        if (validationForm()) {
+
+            btnCommande.textContent = "Veuillez patienter ..."
+            const formValid = validationForm()
+            if (formValid !== false) {
+                const response = await postData('POST', 'http://localhost:3000/api/cameras/order', cartInformation)
+                //window.setTimeout(function(){location.replace("confirmation.html")}, 2000)
+            }
+        } else {
+            validationForm()
+            btnCommande.textContent = "Veuillez remplir correctement les champs ..."
         }
+        //localStorage.clear()
+        console.log(postData('POST', 'http://localhost:3000/api/cameras/order', cartInformation))
+
+        console.log(cartInformation)
+    })
+
+
+}
 
 articlesPanier()
 
-
-
-
-/*class Client {
-    constructor(prenom, nom, email, adresse, pays, zipcode){
-    this.prenom = 'prenom'
-    this.nom = 'nom'
-    this.email = 'email@g.com'
-    this.adresse = 'adresse'
-    this.pays = 'pays'
-    this.zipcode = '01000'
-         }
-    }
-Client.push(prenom.value, nom.value, email.value, adresse.value, pays.value, codePostal.value)*/
--- javascript/produit.js
+++ javascript/produit.js
@@ -12,126 +12,173 @@ const getProduct = async function () {
     if (response.ok) {
       let data = await response.json();
       console.log(data)
-    
 
-      
+
+
       function templateItems() {
 
-        
+
 
         // Ajout de la Div Col dans la div row items
         const cardItems = document.getElementById("items")
-          const divCol = document.createElement('div');
-          divCol.setAttribute('class', 'col-12')
-          cardItems.appendChild(divCol)
+        const divCol = document.createElement('div');
+        divCol.setAttribute('class', 'col-12')
+        cardItems.appendChild(divCol)
 
-          // Creation div card dans la div col
-          const card = document.createElement('div');
-          card.setAttribute('class', 'card')
-          divCol.appendChild(card)
+        // Creation div card dans la div col
+        const card = document.createElement('div');
+        card.setAttribute('class', 'card')
+        divCol.appendChild(card)
 
-          // Creation div card body dans la div card
-          const cardBody = document.createElement('div');
-          cardBody.setAttribute('class', 'card-body')
-          card.appendChild(cardBody)
+        // Creation div card body dans la div card
+        const cardBody = document.createElement('div');
+        cardBody.setAttribute('class', 'card-body')
+        card.appendChild(cardBody)
 
-          // Creation Div title (+ ajout du titre)  dans la div card body
-          const cardTitle = document.createElement('div');
-          cardTitle.setAttribute('class', 'card-title')
-          cardBody.appendChild(cardTitle)
-          const titleProd = document.createElement('h5')
-          titleProd.textContent = data.name
-          cardTitle.appendChild(titleProd)
+        // Creation Div title (+ ajout du titre)  dans la div card body
+        const cardTitle = document.createElement('div');
+        cardTitle.setAttribute('class', 'card-title')
+        cardBody.appendChild(cardTitle)
+        const titleProd = document.createElement('h5')
+        titleProd.textContent = data.name
+        cardTitle.appendChild(titleProd)
 
-          // Creation div text/description (+ ajout du prix + ajout du bouton acheter) dans la div cardItems
-          const cardText = document.createElement('div');
-          cardText.setAttribute('class', 'card-text')
-          cardTitle.appendChild(cardText)
-          const desProd = document.createElement('p')
-          const priProd = document.createElement('p')
-          desProd.textContent = data.description
-          priProd.textContent = `${data.price} Francs => ${Math.round(data.price/6.5597)} €`
-          cardText.appendChild(desProd)
-          cardText.appendChild(priProd)
+        // Creation div text/description (+ ajout du prix + ajout du bouton acheter) dans la div cardItems
+        const cardText = document.createElement('div');
+        cardText.setAttribute('class', 'card-text')
+        cardTitle.appendChild(cardText)
+        const desProd = document.createElement('p')
+        const priProd = document.createElement('p')
+        desProd.textContent = data.description
+        priProd.textContent = `${data.price} Francs => ${Math.round(data.price / 6.5597)} €`
+        cardText.appendChild(desProd)
+        cardText.appendChild(priProd)
 
-          
-          //ajout choix lenses via form
-          const form = document.createElement("form"); 
-	        form.setAttribute("id", "formSelect");
 
-	        const selectLenses = document.createElement("select"); // ON CREE UN ELEMENT SELECT
-	        selectLenses.setAttribute("class", "form-control custom-select mb-4");
-	        selectLenses.setAttribute("required", ""); // CHOIX OBLIGATOIRE
+        //ajout choix lenses via form
+        const form = document.createElement("form");
+        form.setAttribute("id", "formSelect");
 
-	        let optionTitle = document.createElement("option"); // ON CREE UNE PREMIERE OPTION QUI SERVIRA D'INFORMATION
-	        optionTitle.setAttribute("selected", ""); // ON CHOISI LA PREMIERE OPTION DE BASE
-	        optionTitle.setAttribute("disabled", ""); // ON LA DESACTIVE AFIN QU'ELLE NE SOIT PAS SELECTIONNABLE
-	        optionTitle.setAttribute("value", "");
-          optionTitle.textContent = "Choisissez la taille de la lentille:";
-          cardText.appendChild(form)
-          form.appendChild(selectLenses);
-          selectLenses.appendChild(optionTitle)
+        const selectLenses = document.createElement("select"); // ON CREE UN ELEMENT SELECT
+        selectLenses.setAttribute("class", "form-control custom-select mb-4");
+        selectLenses.setAttribute("required", ""); // CHOIX OBLIGATOIRE
 
-          let lenses = data.lenses;
-          for (let lense of lenses) {
-              let choixLentille = document.createElement('option');
-              choixLentille.textContent = lense;
-              selectLenses.appendChild(choixLentille);
-          } 
-        
-          
-          // Ajout Bouton Ajouter au panier
-          const butProd = document.createElement('button')
-          butProd.setAttribute('class', 'float-right btn btn-warning')
-          butProd.textContent = 'Ajouter au panier'         
-          cardText.appendChild(butProd)
+        let optionTitle = document.createElement("option"); // ON CREE UNE PREMIERE OPTION QUI SERVIRA D'INFORMATION
+        optionTitle.setAttribute("selected", ""); // ON CHOISI LA PREMIERE OPTION DE BASE
+        optionTitle.setAttribute("disabled", ""); // ON LA DESACTIVE AFIN QU'ELLE NE SOIT PAS SELECTIONNABLE
+        optionTitle.setAttribute("value", "");
+        optionTitle.textContent = "Choisissez la taille de la lentille:";
+        cardText.appendChild(form)
+        form.appendChild(selectLenses);
+        selectLenses.appendChild(optionTitle)
 
-          
-          butProd.addEventListener('click', function(e){
-            if(selectLenses.value === ""){
-              butProd.textContent= "Selectionnez une lentille !"
-            } else {
-            if(quantity <= 0, quantity++){
-            }
-          localStorage.setItem(`${data._id} +${quantity}`, JSON.stringify(prodInfo))
-          butProd.textContent = `Ajouté ! (${quantity})`
-          //window.setTimeout(function(){location.reload()}, 2000)
+        let lenses = data.lenses;
+        for (let lense of lenses) {
+          let choixLentille = document.createElement('option');
+          choixLentille.textContent = lense;
+          selectLenses.appendChild(choixLentille);
+        }
+
+
+        // Ajout Bouton Ajouter au panier
+        const butProd = document.createElement('button')
+        butProd.setAttribute('class', 'float-right btn btn-warning')
+        butProd.textContent = 'Ajouter au panier'
+        cardText.appendChild(butProd)
+
+
+
+
+        butProd.addEventListener('click', function () {
+          if(selectLenses.value === ""){
+            butProd.textContent= "Selectionnez une lentille !"
+          } else {
+          if (quantity <= 0, quantity++) {
           }
-          })
-        
-          /*function totalPriceInCart(){
-            let totalPriceInCart = localStorage.getItem("totalPriceInCart");
-            totalPriceInCart = JSON.parse(totalPriceInCart);
-            totalPriceInCart = totalPriceInCart + Math.round(data.price/6.5597);
-            localStorage.setItem("totalPriceInCart", totalPriceInCart);
+          addToCart(data)
+          butProd.textContent = `Ajouté ! +${quantity}`}
+        })
+
+
+        /*function totalPriceInCart(){
+          let totalPriceInCart = localStorage.getItem("totalPriceInCart");
+          totalPriceInCart = JSON.parse(totalPriceInCart);
+          totalPriceInCart = totalPriceInCart + Math.round(data.price/6.5597);
+          localStorage.setItem("totalPriceInCart", totalPriceInCart);
+        }
+  
+        totalPriceInCart();*/
+        //let totalPriceInCart = [data.price]
+        let quantity = 0
+
+        const prodInfo = [data._id, data.name, data.price]
+
+        // fonction Ajout au panier avec gestion de la quantité 
+
+        function addToCart(data) {
+          let cartProducts = []
+          let saveToCartProduct = {
+            _id: data._id,
+            name: data.name,
+            price: data.price,
+            quantity: 1,
+            selectLenses: selectLenses.value
           }
-    
-          totalPriceInCart();*/
-          //let totalPriceInCart = [data.price]
-          let quantity = 0
-          const prodInfo = [data._id, data.name, data.price]
-         
-         
-         
+          let newDifferentProduct = true;
 
-          // Ajout de l'image dans la div card text
-          const cardImg = document.createElement('img');
-          cardImg.setAttribute('class', 'card-img mt-3')
-          cardText.appendChild(cardImg)
-          const imgProd = data.imageUrl
-          cardImg.setAttribute('src', imgProd) 
-
-          
-          const lienPanier = document.getElementById("lienpanier")
-          if(localStorage.length === 0){
-            lienPanier.setAttribute('href', 'paniervide.html')
+          if (localStorage.getItem('cartProducts') === null) {
+            cartProducts.push(saveToCartProduct);
+            localStorage.setItem('cartProducts', JSON.stringify(cartProducts))
           }
-          console.log(localStorage)
-        } 
+          else {
+            cartProducts = JSON.parse(localStorage.getItem('cartProducts'))
 
-        templateItems()
 
-      } else {
+            cartProducts.forEach((prod) => {
+              if (data._id === prod._id && data.selectLenses === prod.selectlenses) {
+                prod.quantity++;
+                newDifferentProduct = false;
+              }
+            })
+            if (newDifferentProduct) cartProducts.push(saveToCartProduct);
+            localStorage.setItem('cartProducts', JSON.stringify(cartProducts))
+
+          }
+          checkCart()
+        }
+// Fonction de controle du panier (possibilité afficher quantité total dans panier)
+        function checkCart() {
+          let nb = 0;
+
+          if (localStorage.getItem('cartProducts') !== null) {
+            let cp = JSON.parse(localStorage.getItem("cartProducts"));
+
+            cp.forEach((prod) => {
+              nb = nb + prod.quantity;
+            });
+          }
+console.log(nb)
+        }
+
+
+        // Ajout de l'image dans la div card text
+        const cardImg = document.createElement('img');
+        cardImg.setAttribute('class', 'card-img mt-3')
+        cardText.appendChild(cardImg)
+        const imgProd = data.imageUrl
+        cardImg.setAttribute('src', imgProd)
+
+
+        const lienPanier = document.getElementById("lienpanier")
+        if (localStorage.length === 0) {
+          lienPanier.setAttribute('href', 'paniervide.html')
+        }
+        console.log(localStorage)
+      }
+
+      templateItems()
+
+    } else {
       console.error('reponse serveur : ', response.status);
     }
   } catch (e) { //catch permet de capturer l'erreur
